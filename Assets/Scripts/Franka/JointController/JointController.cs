15a024df9450e57c29621c2770937363ad8c8d65
LOOP <54584749+LOOP115@users.noreply.github.com>
2024-04-18 00:29:20 +1000

    Update JointController.cs

--- Assets/Scripts/Franka/JointController/JointController.cs
+++ Assets/Scripts/Franka/JointController/JointController.cs
@@ -9,6 +9,7 @@ namespace Franka.Control
 {
     public class JointController : MonoBehaviour
     {
+        private ArticulationBody[] articulationBodies;
         private ArticulationBody[] articulationChain;
         // Stores original colors of the part being highlighted
         private Color[] prevColor;
@@ -16,6 +17,7 @@ namespace Franka.Control
 
         [InspectorReadOnly(hideInEditMode: true)]
         public string selectedJoint;
+        
         [HideInInspector]
         public int selectedIndex;
 
@@ -35,11 +37,11 @@ namespace Franka.Control
 
         void Start()
         {
-            previousIndex = selectedIndex = 1;
+            previousIndex = selectedIndex = 0;
             this.gameObject.AddComponent<FKRobot>();
-            articulationChain = this.GetComponentsInChildren<ArticulationBody>();
+            articulationBodies = this.GetComponentsInChildren<ArticulationBody>();
             int defDyanmicVal = 10;
-            foreach (ArticulationBody joint in articulationChain)
+            foreach (ArticulationBody joint in articulationBodies)
             {
                 joint.gameObject.AddComponent<FrankaJointControl>();
                 joint.jointFriction = defDyanmicVal;
@@ -48,7 +50,15 @@ namespace Franka.Control
                 currentDrive.forceLimit = forceLimit;
                 joint.xDrive = currentDrive;
             }
-            DisplaySelectedJoint(selectedIndex);
+
+            articulationChain = new ArticulationBody[FrankaConstants.NumJoints];
+            var linkName = string.Empty;
+            for (var i = 0; i < FrankaConstants.NumJoints; i++)
+            {
+                linkName += FrankaConstants.LinkNames[i];
+                articulationChain[i] = transform.Find(linkName).GetComponent<ArticulationBody>();
+            }
+            // DisplaySelectedJoint(selectedIndex);
             StoreJointColors(selectedIndex);
         }
 
@@ -90,6 +100,17 @@ namespace Franka.Control
         public void setControllerState(bool state)
         {
             controllerActive = state;
+            if (controllerActive)
+            {
+                previousIndex = 6;
+                selectedIndex = 0;
+                StoreJointColors(previousIndex);
+                Highlight(selectedIndex);
+            }
+            else
+            {
+                ResetJointColors(selectedIndex);
+            }
         }
 
 
@@ -110,14 +131,17 @@ namespace Franka.Control
             // store colors for the current selected joint
             StoreJointColors(selectedIndex);
 
-            DisplaySelectedJoint(selectedIndex);
-            Renderer[] rendererList = articulationChain[selectedIndex].transform.GetChild(0).GetComponentsInChildren<Renderer>();
+            // DisplaySelectedJoint(selectedIndex);
+            Renderer[] rendererList = articulationChain[selectedIndex].transform.Find("Visuals").GetComponentsInChildren<Renderer>();
 
             // set the color of the selected join meshes to the highlight color
             foreach (var mesh in rendererList)
             {
-                MaterialExtensions.SetMaterialColor(mesh.material, highLightColor);
+                Color originalColor = MaterialExtensions.GetMaterialColor(mesh);
+                Color adjustedColor = Color.Lerp(originalColor, highLightColor, 0.5f);
+                MaterialExtensions.SetMaterialColor(mesh.material, adjustedColor);
             }
+
         }
 
         void DisplaySelectedJoint(int selectedIndex)
@@ -127,6 +151,45 @@ namespace Franka.Control
                 return;
             }
             selectedJoint = articulationChain[selectedIndex].name + " (" + selectedIndex + ")";
+        }
+
+        /// <summary>
+        /// Stores original color of the part being highlighted
+        /// </summary>
+        /// <param name="index">Index of the part in the Articulation chain</param>
+        private void StoreJointColors(int index)
+        {
+            Renderer[] materialLists = articulationChain[index].transform.Find("Visuals").GetComponentsInChildren<Renderer>();
+            prevColor = new Color[materialLists.Length];
+            for (int counter = 0; counter < materialLists.Length; counter++)
+            {
+                prevColor[counter] = MaterialExtensions.GetMaterialColor(materialLists[counter]);
+            }
+        }
+
+        /// <summary>
+        /// Resets original color of the part being highlighted
+        /// </summary>
+        /// <param name="index">Index of the part in the Articulation chain</param>
+        private void ResetJointColors(int index)
+        {
+            Renderer[] previousRendererList = articulationChain[index].transform.Find("Visuals").GetComponentsInChildren<Renderer>();
+            for (int counter = 0; counter < previousRendererList.Length; counter++)
+            {
+                MaterialExtensions.SetMaterialColor(previousRendererList[counter].material, prevColor[counter]);
+            }
+        }
+
+        public void UpdateControlType(FrankaJointControl joint)
+        {
+            joint.controltype = control;
+            if (control == ControlType.PositionControl)
+            {
+                ArticulationDrive drive = joint.joint.xDrive;
+                drive.stiffness = stiffness;
+                drive.damping = damping;
+                joint.joint.xDrive = drive;
+            }
         }
 
         /// <summary>
@@ -168,51 +231,13 @@ namespace Franka.Control
             }
         }
 
-        /// <summary>
-        /// Stores original color of the part being highlighted
-        /// </summary>
-        /// <param name="index">Index of the part in the Articulation chain</param>
-        private void StoreJointColors(int index)
-        {
-            Renderer[] materialLists = articulationChain[index].transform.GetChild(0).GetComponentsInChildren<Renderer>();
-            prevColor = new Color[materialLists.Length];
-            for (int counter = 0; counter < materialLists.Length; counter++)
-            {
-                prevColor[counter] = MaterialExtensions.GetMaterialColor(materialLists[counter]);
-            }
-        }
-
-        /// <summary>
-        /// Resets original color of the part being highlighted
-        /// </summary>
-        /// <param name="index">Index of the part in the Articulation chain</param>
-        private void ResetJointColors(int index)
-        {
-            Renderer[] previousRendererList = articulationChain[index].transform.GetChild(0).GetComponentsInChildren<Renderer>();
-            for (int counter = 0; counter < previousRendererList.Length; counter++)
-            {
-                MaterialExtensions.SetMaterialColor(previousRendererList[counter].material, prevColor[counter]);
-            }
-        }
-
-        public void UpdateControlType(FrankaJointControl joint)
-        {
-            joint.controltype = control;
-            if (control == ControlType.PositionControl)
-            {
-                ArticulationDrive drive = joint.joint.xDrive;
-                drive.stiffness = stiffness;
-                drive.damping = damping;
-                joint.joint.xDrive = drive;
-            }
-        }
-
-        public void OnGUI()
-        {
-            GUIStyle centeredStyle = GUI.skin.GetStyle("Label");
-            centeredStyle.alignment = TextAnchor.UpperCenter;
-            GUI.Label(new Rect(Screen.width / 2 - 200, 10, 400, 20), "Press left/right arrow keys to select a robot joint.", centeredStyle);
-            GUI.Label(new Rect(Screen.width / 2 - 200, 30, 400, 20), "Press up/down arrow keys to move " + selectedJoint + ".", centeredStyle);
-        }
+        // public void OnGUI()
+        // {
+        //     GUIStyle centeredStyle = GUI.skin.GetStyle("Label");
+        //     centeredStyle.alignment = TextAnchor.UpperCenter;
+        //     GUI.Label(new Rect(Screen.width / 2 - 200, 10, 400, 20), "Press left/right arrow keys to select a robot joint.", centeredStyle);
+        //     GUI.Label(new Rect(Screen.width / 2 - 200, 30, 400, 20), "Press up/down arrow keys to move " + selectedJoint + ".", centeredStyle);
+        // }
     }
+
 }
